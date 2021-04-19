0b28128a7279f7d067f59c6eb644c733d4fc6d8d
BladeMight <BladeMight@gmail.com>
2021-04-19 13:00:23 +0300

    :memo: Mahou.mm dir: lnk *correct*? icon.

--- Mahou/MahouUI.cs
+++ Mahou/MahouUI.cs
@@ -4989,6 +4989,52 @@ DEL ""ExtractASD.cmd""";
 			};
 			wfm.Start();
 		}
+		static string GetShortcutTarget(string file) {
+		    try {
+		        if (Path.GetExtension(file).ToLower() != ".lnk") {
+		            throw new Exception("Supplied file must be a .LNK file");
+		        }
+		        FileStream fileStream = File.Open(file, FileMode.Open, FileAccess.Read);
+		        using (BinaryReader fileReader = new BinaryReader(fileStream)) {
+		            fileStream.Seek(0x14, SeekOrigin.Begin);     // Seek to flags
+		            uint flags = fileReader.ReadUInt32();        // Read flags
+		            if ((flags & 1) == 1) {                      // Bit 1 set means we have to
+		                                                         // skip the shell item ID list
+		                fileStream.Seek(0x4c, SeekOrigin.Begin); // Seek to the end of the header
+		                uint offset = fileReader.ReadUInt16();   // Read the length of the Shell item ID list
+		                fileStream.Seek(offset, SeekOrigin.Current); // Seek past it (to the file locator info)
+		            }
+		 
+		            long fileInfoStartsAt = fileStream.Position; // Store the offset where the file info
+		            // structure begins
+		            uint totalStructLength = fileReader.ReadUInt32(); // read the length of the whole struct
+		            fileStream.Seek(0xc, SeekOrigin.Current); // seek to offset to base pathname
+		            uint fileOffset = fileReader.ReadUInt32(); // read offset to base pathname
+		            // the offset is from the beginning of the file info struct (fileInfoStartsAt)
+		            fileStream.Seek((fileInfoStartsAt + fileOffset), SeekOrigin.Begin); // Seek to beginning of
+		            // base pathname (target)
+		            long pathLength = (totalStructLength + fileInfoStartsAt) - fileStream.Position - 2; // read
+		            // the base pathname. I don't need the 2 terminating nulls.
+		            char[] linkTarget = fileReader.ReadChars((int) pathLength); // should be unicode safe
+		            var link = new string(linkTarget);
+		 
+		            int begin = link.IndexOf("\0\0");
+		            if (begin > -1) {
+		                int end = link.IndexOf("\\\\", begin + 2) + 2;
+		                end = link.IndexOf('\0', end) + 1;
+		 
+		                string firstPart = link.Substring(0, begin);
+		                string secondPart = link.Substring(end);
+		 
+		                return firstPart + secondPart;
+		            } else {
+		                return link;
+		            }
+		        }
+		    } catch {
+		        return "";
+		    }
+		}
 		static Dictionary<string, Image> file_icons_cache = new Dictionary<string, Image>();
 		static void dirparser(ref ToolStripMenuItem root, string dir, int max_depth, string allow_types, int maxentries, int this_depth=-1) {
 			Debug.WriteLine("parsing: " +dir);
@@ -5044,13 +5090,23 @@ DEL ""ExtractASD.cmd""";
 				}
 				Image img = null;
 				if (!directory) {
-					if (file_icons_cache.ContainsKey(ext)) {
-						img = file_icons_cache[ext];
+					var eex = ext;
+					var ffd = fd;
+					if (ext.ToLower() == ".lnk") {
+						ffd = GetShortcutTarget(fd);
+						if (File.Exists(ffd)) {
+							eex = new FileInfo(ffd).Extension;
+						} else {
+							ffd = fd;
+						}
+					}
+					if (file_icons_cache.ContainsKey(eex)) {
+						img = file_icons_cache[eex];
 					} else {
-						var b = Icon.ExtractAssociatedIcon(fd).ToBitmap();
+						var b = Icon.ExtractAssociatedIcon(ffd).ToBitmap();
 						img = Image.FromHbitmap(b.GetHbitmap());
 						b.Dispose();
-						file_icons_cache[ext] = img;
+						file_icons_cache[eex] = img;
 					}
 				} 
 				else {
